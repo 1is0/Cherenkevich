4fc5bb095832381a2f8eb49e1631afa8f4db51ab
V14dik <62301196+V14dik@users.noreply.github.com>
2020-05-13 19:26:21 +0300

    Add files via upload

---
+++ C#/lab 2/8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zad8
+{
+        class MainProgramm
+        {
+            static void Main()
+            {
+                Functions func = new Functions();
+
+                Console.WriteLine("Zadanie 8");
+
+                while (!func.ValidateInfo())
+                {
+                    Console.WriteLine("Язык введен не в верном формате");
+                }
+
+                func.Translate();
+            }
+
+        }
+    }
