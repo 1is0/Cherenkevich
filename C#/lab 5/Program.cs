9b60e69c7d95913c887d09a40f6c528f466f6170
V14dik <62301196+V14dik@users.noreply.github.com>
2020-05-23 10:58:51 +0300

    Add files via upload

---
+++ C#/lab 5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AutoShow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logan Log = new Logan(Car.Color.Grey, 3000,10000);
+            Console.Write(Log);
+            Log.HorsesOnKg();
+        }
+    }
+}
