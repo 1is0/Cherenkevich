2f5df89c101285656f8e39fe9991aa7b74ced93e
V14dik <cherenkevich.vlad@gmail.com>
2020-06-05 13:00:19 +0300

    Add

---
+++ C#/lab 8/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace lab8
+{
+    class Car: Transport, IEquipment
+    {
+        public struct Information
+        {
+            public int price;
+            public int mileage;
+        }
+        public Information Inf;
+
+        public string privod { get; set; }
+        public string tipeOfFuel { get; set; }
+        public double Capacity { get; set; }
+        public int HorsePower { get; set; }
+
+        //Добавления в 6 лабораторной
+        public override void RightOrLeftRudder()
+        {
+            Console.WriteLine("Car is left-hand drive");
+        }
+        public override void Equipment()
+        {
+            Console.WriteLine("The car has full kit");
+        }
+        public Car():base()
+        {
+            privod = "Back";
+            tipeOfFuel = "Dizel";
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "; " + "Privod: " + privod + "; ";
+        }
+        public override void BodyTipe()
+        {
+            Console.WriteLine("Body Tipe is sedan");
+        }
+    }
+}
---
+++ C#/lab 8/IEquipment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab8
+{
+    interface IEquipment
+    {
+        void Equipment();
+        void RightOrLeftRudder();
+    }
+}
